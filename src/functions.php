e757062989dfc5b4904e59d55feac96d9b2b4dd1
David Mattsson <mattsson.david@gmail.com>
2018-04-25 00:12:35 +0200

    Disable some junk in wp_head and wp_footer actions

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,19 @@
 <?php
+add_theme_support('post-thumbnails');
+
+/* Remove junk from wp_head() */
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'wp_resource_hints', 2);
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+remove_action( 'wp_head', 'rest_output_link_wp_head');
+
+/* Remove junk in wp_footer() */
+add_action( 'wp_footer', function () {
+    wp_deregister_script('wp-embed');
+});
