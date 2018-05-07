14edf9960c3866ce06db6eb5fef68a215adf7b52
David Mattsson <mattsson.david@gmail.com>
2018-05-07 23:55:23 +0200

    Add some JS modules

---
+++ src/js/API/Post.mjs
@@ -0,0 +1,20 @@
+import { WPRestHome } from '../Util/WPRestHome.mjs';
+
+export function getPosts() {
+    const path = 'wp/v2/posts';
+    if (WPRestHome === undefined) {
+        return undefined;
+    }
+
+    return fetch(WPRestHome + path)
+        .then(response => {
+            const contentType = response.headers.get('content-type');
+            if ((contentType + "").includes('application/json')) {
+                return response;
+            }
+            throw new TypeError('Wrong type');
+        })
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
+};
