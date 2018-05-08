1174587af17250d01927a290d90b504493e0dbe6
David Mattsson <mattsson.david@gmail.com>
2018-05-09 01:19:34 +0200

    Initial add Teaser

---
+++ src/js/Teaser/Teaser.mjs
@@ -0,0 +1,24 @@
+import customElements from '../Util/CustomElements.mjs';
+import {getPosts} from '../API/Post.mjs';
+
+class Teaser extends HTMLElement {
+    static get observedAttributes() {
+        return [];
+    }
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+    }
+
+    disconnectedCallback() {
+    }
+
+    attributeChangedCallback(attrName, oldValue, newValue) {
+    }
+}
+customElements.define('newsy-teaser', Teaser);
+
+export default Teaser;
