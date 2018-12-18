aebd1f235b4519a4a98692a4d1e08d3c03c457dd
David Mattsson <mattsson.david@gmail.com>
2018-12-18 23:35:41 +0100

    cleanup in test

--- src/js/API/Post/Action/Post.test.mjs
+++ src/js/API/Post/Action/Post.test.mjs
@@ -1,11 +1,11 @@
 import chai from 'chai';
 import tap from 'tap';
-import { requestPost, REQUEST_POST } from './Post';
+import { requestPost, REQUEST_POST } from './Post.mjs';
 
-const { mocha } = tap;
+const { describe, it } = tap.mocha;
 const { expect } = chai;
-mocha.describe('Action.Post', () => {
-    mocha.it('creates an action for requesting post', () => {
+describe('Action.Post', () => {
+    it('creates an action for requesting post', () => {
         const expectedAction = {
             id: 1,
             type: REQUEST_POST
