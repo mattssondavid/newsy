ec976228fee68ebdd0e5321231de848dacfc6396
David Mattsson <mattsson.david@gmail.com>
2019-11-19 23:46:59 +0100

    Import correctly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {} from './js/Component/Header/Header.js';
-import {} from './js/Component/Hero/Hero.js';
-import {} from './js/Component/Teaser/Teaser.js';
-import {} from './js/Component/ProgressiveImage/ProgressiveImage.js';
+import './js/Component/Header/Header.js';
+import './js/Component/Hero/Hero.js';
+import './js/Component/Teaser/Teaser.js';
+import './js/Component/ProgressiveImage/ProgressiveImage.js';
 
 import { configureStore } from './js/API/Store/Store.js';
 import { requestPost, requestPosts } from './js/API/Post/Action/Post.js';
@@ -12,7 +12,7 @@ store.dispatch(requestPost(44));
 store.dispatch(requestPosts({ 'order': 'desc', beta:1 }));
 
 
-import {} from './js/Util/WebComponent/HelloWorld.native.js';
-import {} from './js/Util/WebComponent/HelloWorld.template.js';
-import {} from './js/Util/WebComponent/HelloWorld.lithtml.js';
-import {} from './js/Util/WebComponent/HelloWorld.typescript.js';
+import './js/Util/WebComponent/HelloWorld.native.js';
+import './js/Util/WebComponent/HelloWorld.template.js';
+import './js/Util/WebComponent/HelloWorld.lithtml.js';
+import './js/Util/WebComponent/HelloWorld.typescript.js';
